6486941cbd2919c0e403d575ea1cfb2fad13f74a
Chris Pucknell <chris.pucknell@gmail.com>
2023-10-22 14:32:05 +0100

    add update button data functionality

--- test.py
+++ test.py
@@ -28,29 +28,33 @@ def main():
             widgets.destroy()
         for widgets in controlFrame2.winfo_children():
             widgets.destroy()
-        createControlButtons()
         
     def switchGroup(buttonID):
         removeButtons()
         createGroupButtons(buttonID)
 
-    def backButton(groupID):
+    def backButton(groupID=""):
+        global data
         removeButtons()
-        parentID = data[groupID]['parent']
-        if (parentID != ""):
-            grandParentID = data[parentID]['parent']
+        if groupID:
+            parentID = data[groupID]['parent']
+            if (parentID != ""):
+                grandParentID = data[parentID]['parent']
+            else:
+                grandParentID = ""
+            if (grandParentID == ""):
+                createFirstGroupButtons()
+            else:
+                createGroupButtons(grandParentID)
         else:
-            grandParentID = ""
-        if (grandParentID == ""):
             createFirstGroupButtons()
-        else:
-            createGroupButtons(grandParentID)
-
-    # CREATE BUTTONS
-
+            
+#################################################################################################################################
+# CREATE BUTTONS ################################################################################################################ 
+#################################################################################################################################
     def createButton(buttonID):
         global btnCount
-
+        global data
         # INCREMENT COUNT
         btnCount = btnCount + 1
 
@@ -60,7 +64,7 @@ def main():
         btnLabel.grid(row=btnCount-1,column=0,sticky=tk.W+tk.E,pady=2)
 
         # EDIT BUTTON
-        editBtn = ttk.Button(buttonFrame, text="🖊",width=0, command=lambda:createForm(buttonID))
+        editBtn = ttk.Button(buttonFrame, text="🖊",style="Center.TButton",width=0,command=lambda:createForm(buttonID))
         editBtn.grid(row=btnCount-1,column=2,pady=2,padx=4)
         
         # BUTTON WITH COMMAND
@@ -80,8 +84,11 @@ def main():
         btn = ttk.Button(controlFrame, text="Options", command=lambda : popupmsg("test"))
         btn.grid(row=0,column=1,padx=10,sticky="W")
 
-    def createBackButton(grandParentID):
-        btn = ttk.Button(controlFrame2, text="Back", command=lambda : backButton(grandParentID))
+    def createBackButton(grandParentID=""):
+        if grandParentID:
+            btn = ttk.Button(controlFrame2, text="Back", command=lambda : backButton(grandParentID))
+        else:
+            btn = ttk.Button(controlFrame2, text="Back", command=lambda : backButton())
         root.bind('b',lambda event, button=btn: btn.invoke())
         btn.grid(row=0,column=0,sticky="E")
 
@@ -90,38 +97,55 @@ def main():
     #     spacer_label.grid(row=0, column=0)
 
     def createFirstGroupButtons():
-        for k in data.keys():
-            if (data[k]['parent'] == ""):
-                createButton(k)
+        global data
+        createControlButtons()
+        if data:
+            for k in data.keys():
+                if (data[k]['parent'] == ""):
+                    createButton(k)
 
     def createGroupButtons(groupID):
-        if (data[groupID]['group']):
-            createBackButton(groupID)
-            for k in data.keys():
-                if (data[k]['parent'] == groupID):
-                    createButton(k)
-        #further logic to come for action when not a group?
+        global data
+        createControlButtons()
+        if data:
+            if (data[groupID]['group']):
+                createBackButton(groupID)
+                for k in data.keys():
+                    if (data[k]['parent'] == groupID):
+                        createButton(k)
+            #further logic to come for action when not a group?
 
     def createControlButtons():
         # createSpacer()
         createAddButton()
         createOptionsButton()
 
-    ## NEW/EDIT DATA FORM
+    def createSubmitButton(buttonID,e1,e2,parentID):
+        btn = ttk.Button(controlFrame,text="Submit",command=lambda: submitForm(buttonID,e1,e2,parentID))
+        btn.grid(row=0,column=0)
+# ==========================================================================================
+# NEW/EDIT DATA FORM  ======================================================================
+# ==========================================================================================
     def createForm(buttonID=""):
+        global data
         removeButtons()
         buttonName=""
         buttonContent=""
         group = False
+        parentID=""
         chkvar=tk.IntVar()
+        chkvar.set(0)
         if buttonID:
             buttonName=data[buttonID]['label']
             buttonContent=data[buttonID]['content']
+            parentID = data[buttonID]['parent']
             if (data[buttonID]['group']):
                 group=True
                 chkvar.set(1)
-            
-
+        else:
+            newID = uuid.uuid1()
+            buttonID = newID
+                        
         def checkboxFunction():
             global group
             if(chkvar.get()==0):
@@ -130,32 +154,15 @@ def main():
             elif(chkvar.get()==1):
                 e2.config( state ='disabled') 
                 group = True
-    # button name
-        # field label
+                
         l1 = tk.Label(buttonFrame,text="Button Name: ")
-        # input field
-        e1 = ttk.Entry(
-            buttonFrame,
-            width=100
-        )
+        e1 = ttk.Entry(buttonFrame,width=100)
         e1.insert(0,buttonName)
-        
-    # button current content
-        # field label
         l2 = tk.Label(buttonFrame,text="Content: ")
-        # input field
-
-        e2 = ttk.Entry(
-            buttonFrame,
-            width=100
-        )
+        e2 = ttk.Entry(buttonFrame,width=100)
         e2.insert(0,buttonContent)
-        
-    # is a group?
         l3 = tk.Label(buttonFrame,text="Button Group: ")
         ch = ttk.Checkbutton(buttonFrame,variable=chkvar, onvalue=1, offvalue=0, command=checkboxFunction)
-        
-    # dropdown menu to select groupw
         l4 = tk.Label(buttonFrame,text="Location: ")
         dp = tk.Listbox(buttonFrame)
     
@@ -171,15 +178,42 @@ def main():
         
         l4.grid(row=3,column=0,sticky="NE")
         dp.grid(row=3,column=1,sticky="W")
+        createSubmitButton(buttonID,e1,e2,parentID)
+        createBackButton()
 
+    def submitForm(buttonID,e1,e2,parentID):
+        global data
+        data[buttonID]["parent"]=parentID
+
+        if parentID:
+            data[buttonID]["group"]=True
+        else:
+            data[buttonID]["group"]=False
+        data[buttonID]["label"]=e1.get()
+        data[buttonID]["content"]=e2.get()
+        print("test1",type(data))
+        writeData(data) 
+        
 
 # ==========================================================================================
 # SETUP AND LAUNCH =========================================================================
 # ==========================================================================================
-
-    with open("data.json","r") as f:
-        data = json.load(f)
-
+    data = dict()
+    
+    def readData():
+        global data
+        with open("data.json","r") as f:
+            data = json.load(f)
+            
+    def writeData(data):
+        print(type(data))
+        json_data = json.dumps(data,indent=4)
+        print(type(json_data))
+        with open("data.json","w", encoding='utf-8') as json_file:
+            json_file.write(json_data)
+            
+    readData()
+    print(data)
     root = tk.Tk()
     root.geometry("500x500")
     root.title("test")
@@ -197,8 +231,8 @@ def main():
 
     header = tk.Frame(
                 root, 
-        borderwidth=1, 
-        relief="solid"
+        # borderwidth=1, 
+        # relief="solid"
     )
     header.grid(
         row=0,
@@ -211,8 +245,8 @@ def main():
     
     body = tk.Frame(
                 root, 
-        borderwidth=1, 
-        relief="solid"
+        # borderwidth=1, 
+        # relief="solid"
     )
     body.grid(
         row=1,
@@ -227,16 +261,16 @@ def main():
         header, 
         text="Snippet Stockpile", 
         font=('Arial', 18, 'bold'), 
-        borderwidth=1, 
-        relief="solid"
+        # borderwidth=1, 
+        # relief="solid"
     )
     label.grid(row=0, column=0, padx=10, pady=10)
 
     controlFrame = tk.Frame(
         body,
         width=100,
-        borderwidth=1, 
-        relief="solid"
+        # borderwidth=1, 
+        # relief="solid"
     )
     controlFrame.grid(
         row=1,
@@ -259,7 +293,11 @@ def main():
         sticky='e'
     )
     
-    buttonFrame = tk.Frame(body, borderwidth=1, relief="solid")
+    buttonFrame = tk.Frame(
+        body, 
+        # borderwidth=1, 
+        # relief="solid"
+    )
     buttonFrame.grid(
         row=2, 
         column=0,
@@ -274,6 +312,7 @@ def main():
 
     style = ttk.Style()
     style.configure("Custom.TButton",anchor='w')
+    style.configure("Center.TButton",anchor='w',padding=(5,1,5,5))
 
     # BUTTON LOGIC
     root.bind('<Escape>', quit)
@@ -281,7 +320,6 @@ def main():
     # Initial Button Population
     
     createFirstGroupButtons()
-    createControlButtons()
     # createBindings()
     root.mainloop() # this bit makes it render#
 
